c8d772dc6d363f36f7e4f86b0a055775dbb0f67a
alex718 <sasha264905@mail.ru>
2023-03-21 17:37:39 +0300

    resolving git issues

--- service/backup_commit.sh
+++ service/backup_commit.sh
@@ -7,7 +7,10 @@ BASE_DIR="/root/new_horizon_project"
 
 # go to backup branch
 #git branch $backup_branch_name
+<<<<<<< HEAD
 cd $BASE_DIR
+=======
+>>>>>>> 52d9433 ("backup autocommit 2023-03-21 17:37:39")
 git checkout $backup_branch_name
 
 # save changes to backup branch
@@ -17,7 +20,7 @@ git push --set-upstream origin $backup_branch_name
 
 
 # go to main branch and push changes 
-git checkout $main_branch_name
-git merge $backup_branch_name
+#git checkout $main_branch_name
+#git merge $backup_branch_name
 
-git push origin $main_branch_name
+#git push origin $main_branch_name
