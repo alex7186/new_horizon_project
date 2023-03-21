52d9433b21aae76396e5fb7b837270492fc69436
alex718 <sasha264905@mail.ru>
2023-03-21 17:37:39 +0300

    "backup autocommit 2023-03-21 17:37:39"

--- service/backup_commit.sh
+++ service/backup_commit.sh
@@ -1,21 +1,21 @@
-commit_name="backup autocommit $(date +'%Y-%m-%d %H:%M:%S')"
+commit_name="\"backup autocommit $(date +'%Y-%m-%d %H:%M:%S')\""
 
 backup_branch_name="backup"
 main_branch_name="main"
 
 
 # go to backup branch
-git branch $backup_branch_name
+#git branch $backup_branch_name
 git checkout $backup_branch_name
 
 # save changes to backup branch
 git add .
-git commit $commit_name
+git commit -m "${commit_name}"
 git push --set-upstream origin $backup_branch_name
 
 
 # go to main branch and push changes 
-git checkout $main_branch_name
-git merge $backup_branch_name
+#git checkout $main_branch_name
+#git merge $backup_branch_name
 
-git push origin $main_branch_name
+#git push origin $main_branch_name
