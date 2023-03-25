109b76ad30e82b17db0157cd74aeae4f91aeab33
alex718 <sasha264905@mail.ru>
2023-03-25 17:52:03 +0300

    "backup autocommit 2023-03-25 17:52:03"

--- service/backup_commit.sh
+++ service/backup_commit.sh
@@ -7,10 +7,8 @@ BASE_DIR="/root/new_horizon_project"
 
 # go to backup branch
 #git branch $backup_branch_name
-<<<<<<< HEAD
+
 cd $BASE_DIR
-=======
->>>>>>> 52d9433 ("backup autocommit 2023-03-21 17:37:39")
 git checkout $backup_branch_name
 
 # save changes to backup branch
