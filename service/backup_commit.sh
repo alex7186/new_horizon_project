36a9140528ab7290b4a87105361113c57be529c4
alex7186 <sasha2649058@mail.ru>
2023-03-25 17:58:15 +0300

    changed backup commit script

--- service/backup_commit.sh
+++ service/backup_commit.sh
@@ -7,7 +7,7 @@ BASE_DIR="/root/new_horizon_project"
 
 # go to backup branch
 #git branch $backup_branch_name
-
+echo "trying to push changes as ${commit_name}" >> $BASE_DIR/misc/backups.txt
 cd $BASE_DIR
 git checkout $backup_branch_name
 
@@ -18,7 +18,7 @@ git push --set-upstream origin $backup_branch_name
 
 
 # go to main branch and push changes 
-#git checkout $main_branch_name
-#git merge $backup_branch_name
+git checkout $main_branch_name
+git merge $backup_branch_name
 
-#git push origin $main_branch_name
+git push origin $main_branch_name
